6851f6fa14cb596b769b6cea5a28f5e799c9815f
Your Name <wjdgml3834@naver.com>
2022-05-26 18:14:23 +0900

    🐛  fix: fix problems eslint error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 
+/**
+ * App 함수
+ *@return {React.HTMLAttributes<HTMLDivElement>}
+ */
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
