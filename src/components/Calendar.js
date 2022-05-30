5300a3c9a7ced572af7ba673651c732de4ade9dc
KimGaeun0806 <greencloud11@hanyang.ac.kr>
2022-05-30 17:01:54 +0900

    🎨 Feat: Make Calendar function

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import React from 'react';
+import dayjs from 'dayjs';
 
 const Container = styled.div`
   background: linear-gradient(
@@ -33,7 +34,10 @@ const Header = styled.div`
   justify-content: space-between;
 `;
 
-const Month = styled.div``;
+const Month = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+`;
 
 const HUHS = styled.p`
   font-size: 18px;
@@ -54,15 +58,67 @@ const DayOfWeek = styled.p`
   color: #73777b;
 `;
 
+const CalendarDate = styled.div``;
+
+const WeekBox = styled.div`
+  margin: 0px 10px 0px 10px;
+  padding: 7px 11px 0px 11px;
+  border-bottom: 3px solid #e4e4e4;
+  height: 64.8px;
+  justify-content: space-between;
+  display: flex;
+`;
+
+const WeekDate = styled.p``;
+
+// eslint-disable-next-line react/prop-types
+const Week = ({ start }) => {
+  const NumStart = parseInt(start);
+
+  const NowStart = dayjs().startOf('M').format('D'); // 이번달 첫날
+  const NowEnd = dayjs().endOf('M').format('D'); // 이번달 마지막날
+
+  const NowYear = dayjs().format('YYYY'); // 올해
+  const NowMonth = dayjs().format('MM'); // 이번달
+
+  const LastMonthEnd = dayjs(`${NowYear}-${NowMonth - 1}`)
+    .endOf('M')
+    .format('D'); // 저번달 마지막날
+
+  const giveDate = num => {
+    if (num < NowStart) {
+      return parseInt(LastMonthEnd) + num;
+    } else if (num > NowEnd) {
+      return num - NowEnd;
+    } else return num;
+  };
+
+  return (
+    <WeekBox>
+      <WeekDate>{giveDate(NumStart)}</WeekDate>
+      <WeekDate>{giveDate(NumStart + 1)}</WeekDate>
+      <WeekDate>{giveDate(NumStart + 2)}</WeekDate>
+      <WeekDate>{giveDate(NumStart + 3)}</WeekDate>
+      <WeekDate>{giveDate(NumStart + 4)}</WeekDate>
+      <WeekDate>{giveDate(NumStart + 5)}</WeekDate>
+      <WeekDate>{giveDate(NumStart + 6)}</WeekDate>
+    </WeekBox>
+  );
+};
+
 const Calendar = () => {
+  const NowYearAndMonth = dayjs().format(`YYYY년 MM월`); // 이번달
+  const NowStartDay = dayjs().startOf('M').format('d'); // 이번달 첫날 요일
+
   return (
     <Container>
       <OuterLayout>
         <CalendarBox>
           <Header>
-            <Month></Month>
+            <Month>{NowYearAndMonth}</Month>
             <HUHS>HUHS</HUHS>
           </Header>
+
           <DayOfWeekBox>
             <DayOfWeek>SUN</DayOfWeek>
             <DayOfWeek>MON</DayOfWeek>
@@ -72,6 +128,15 @@ const Calendar = () => {
             <DayOfWeek>FRI</DayOfWeek>
             <DayOfWeek>SAT</DayOfWeek>
           </DayOfWeekBox>
+
+          <CalendarDate>
+            <Week start={1 - NowStartDay} />
+            <Week start={1 - NowStartDay + 7} />
+            <Week start={1 - NowStartDay + 14} />
+            <Week start={1 - NowStartDay + 21} />
+            <Week start={1 - NowStartDay + 28} />
+            <Week start={1 - NowStartDay + 35} />
+          </CalendarDate>
         </CalendarBox>
 
         <ScheduleBox></ScheduleBox>
